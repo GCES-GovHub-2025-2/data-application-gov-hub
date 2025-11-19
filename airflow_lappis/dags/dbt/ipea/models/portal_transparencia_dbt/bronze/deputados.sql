21449a9f8e88d0f1d44a532d98c8d8926824f86b
LeoFacB <leonardofbonetti@gmail.com>
2025-11-19 01:32:34 -0300

    feat(bronze): adicionar tratamento de null e limpeza de dados no modelo deputados

--- airflow_lappis/dags/dbt/ipea/models/portal_transparencia_dbt/bronze/deputados.sql
+++ airflow_lappis/dags/dbt/ipea/models/portal_transparencia_dbt/bronze/deputados.sql
@@ -1,23 +1,22 @@
-
 {{ config(materialized="table") }}
 
-with source as (
-    select * from {{ source('camara_deputados', 'deputados') }}
-),
+with
+    source as (select * from {{ source("camara_deputados", "deputados") }}),
 
-renamed as (
-    select
-        cast(id as integer) as id_deputado,
-        uri as uri_deputado,
-        nome as nome_deputado,
-        siglapartido as sigla_partido,
-        uripartido as uri_partido,
-        siglauf as sigla_uf,
-        cast(idlegislatura as integer) as id_legislatura,
-        urlfoto as url_foto,
-        email as email_deputado,
-        cast(dt_ingest as timestamp) as data_ingestao
-    from source
-)
+    renamed as (
+        select
+            cast(nullif(trim(id), '') as integer) as id_deputado,
+            nullif(trim(uri), '') as uri_deputado,
+            nullif(trim(nome), '') as nome_deputado,
+            nullif(trim(siglapartido), '') as sigla_partido,
+            nullif(trim(uripartido), '') as uri_partido,
+            upper(nullif(trim(siglauf), '')) as sigla_uf,
+            cast(nullif(trim(idlegislatura), '') as integer) as id_legislatura,
+            nullif(trim(urlfoto), '') as url_foto,
+            nullif(trim(email), '') as email_deputado,
+            cast(dt_ingest as timestamp) as data_ingestao
+        from source
+    )
 
-select * from renamed
+select *
+from renamed
